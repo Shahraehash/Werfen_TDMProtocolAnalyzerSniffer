3e93f08caeb39dfda95245623f1945e6ad89a3b9
Raehash Shah <rshah1@werfen.com>
2022-07-08 11:57:45 -0700

    Bug Fix: Status-get checkbox resets after closing the session

--- Server_end/recieving_server.py
+++ Server_end/recieving_server.py
@@ -15,12 +15,14 @@ def main(HOST):
         data = conn.recv(122)
         if data == b'':
             global_vars.close_session = True
+            global_vars.hide_status_get = True
             conn.close()
             print("closing the recieving server connection")
             break
         elif data[:4] == b'0x00' and data[-4:] == b'0x01':
             data_decoded = data[4:-4].decode('utf-8')
             json_data_decoded = json.loads(data_decoded)
+            print(json_data_decoded)
             global_vars.number_of_L4s = int(json_data_decoded['number of L4s']) 
             if int(json_data_decoded['Boolean of Get Command']) == 0:
                 global_vars.hide_status_get = False
